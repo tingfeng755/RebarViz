5c8fc8014741f624793a7ddaa7efde1091c81d23
tingfeng755 <tingfeng755@gmail.com>
2026-03-08 10:08:55 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,8 +5,8 @@ const basePath = isProd ? '/RebarViz' : '';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath,
-  assetPrefix: basePath,
+ basePath: '/song',
+assetPrefix: '/song', // 或者 '/song/'，保持原有斜杠格式即可
   images: { unoptimized: true },
   reactCompiler: true,
 };
